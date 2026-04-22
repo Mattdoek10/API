54fd11b4eb39f82f7350d0cf47bec46621a0d3c2
Mattdoek10 <152287577+Mattdoek10@users.noreply.github.com>
2026-04-22 09:54:50 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
+import Node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: Node({ mode: 'standalone' }),    
+
     vite: {
         server: {
             hmr: false
